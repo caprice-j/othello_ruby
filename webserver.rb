50967ab14a6e04d7fc053a3c159832493cb18f8f
caprice-j <skyfly0008@gmail.com>
2014-06-19 15:12:22 +0900

    add HamlHandler

--- webserver.rb
+++ webserver.rb
@@ -1,6 +1,50 @@
 require 'webrick'
+require 'rubygems'
+require 'haml'
 
-# :DocumentRoot => "./"
-server = WEBrick::HTTPServer.new(:Port => 8080)
-trap("INT"){s.shutdown}
-s.start
+class HamlHandler < WEBrick::HTTPServlet::AbstractServlet
+  def initialize(server, name)
+    super
+    @script_filename = name
+  end
+
+  def do_GET(req, res)
+    begin
+      data = open(@script_filename){|io| io.read }
+      res.body = parse_haml(data)
+      res['content-type'] = 'text/html'
+    rescue StandardError => ex
+      raise
+    rescue Exception => ex
+      @logger.error(ex)
+      raise HTTPStatus::InternalServerError, ex.message
+    end
+  end
+
+  alias do_POST do_GET
+
+  private
+    def parse_haml(string)
+      engine = Haml::Engine.new(string,
+        :attr_wrapper => '"',
+        :filename => @script_filename
+      )
+      engine.render
+    end
+end
+
+WEBrick::HTTPServlet::FileHandler.add_handler("haml", HamlHandler)
+
+args = ARGV.join(' ')
+args.gsub!(%r{^http://}, '')
+args = args.split(/[ :]/).compact
+
+server = WEBrick::HTTPServer.new(
+  :Port => args.pop || 8080,
+  # :BindAddress => args.pop || '0.0.0.0',
+  :DocumentRoot => Dir.pwd
+)
+
+trap("INT") { server.shutdown }
+
+server.start
