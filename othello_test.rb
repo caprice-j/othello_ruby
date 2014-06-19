73d69e49da69f5e889ef54e42314da7c6d4075b0
caprice-j <skyfly0008@gmail.com>
2014-06-19 18:14:06 +0900

    disable CDN

--- othello_test.rb
+++ othello_test.rb
@@ -1,25 +1,5 @@
 require "./othello_board.rb"
 require "./othello_AI.rb"
 
-
-$brd = # overwrite
-[
-   [OUT, OUT, OUT, OUT, OUT, OUT, OUT, OUT, OUT, OUT],
-   [OUT,  EM,  EM,  EM,  EM,  EM,  EM,  EM,  EM, OUT],
-   [OUT,  EM,  EM,  EM,  EM,  EM,  EM,  EM,  EM, OUT],
-   [OUT, WHT, BLK, BLK, BLK, BLK, BLK, BLK, BLK, OUT],
-   [OUT, WHT, BLK, BLK, BLK, WHT, BLK, BLK, BLK, OUT],
-   [OUT, WHT, BLK,  EM, WHT, BLK, BLK, BLK, BLK, OUT],
-   [OUT, WHT, BLK, BLK, BLK, BLK, BLK, BLK, BLK, OUT],
-   [OUT, WHT, BLK, BLK, BLK, BLK, BLK, BLK, BLK, OUT],
-   [OUT,  EM,  EM,  EM,  EM,  EM,  EM,  EM,  EM, OUT],
-   [OUT, OUT, OUT, OUT, OUT, OUT, OUT, OUT, OUT, OUT]
-]
-
-# show
-# place [5,3], WHT
-
-show
-rp = RandomAI.new(WHT)
-place(rp.think, rp.cl)
-show
+    game_board = Board.new(6,6)
+game_board.csv
